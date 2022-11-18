accc97d4cf196b4cb840c54b56b97321461fa851
redbalancer <redbalancer@163.com>
2022-11-18 11:43:43 +0800

    新增加的页面访问用户信息的请求处理类

---
+++ src/main/java/org/xxyyzz/control/WebController.java
@@ -0,0 +1,32 @@
+package org.xxyyzz.control;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.xxyyzz.service.UserInfoService;
+
+/**
+ * @author Chunlong Zhang
+ * @version 1.0.0
+ * @ClassName WebController.java
+ * @Description @TODO
+ * @createTime 2022年11月18日 11:00:00
+ */
+
+@RestController
+public class WebController {
+
+    @Autowired
+    @Qualifier( "User_Services" )
+    UserInfoService service;
+
+    @RequestMapping("/user/{name}")
+//    @PreAuthorize()
+    String getUserInfo( @PathVariable( "name" ) String userName) {
+        return service.getUserByName( userName ).toString();
+    }
+}
